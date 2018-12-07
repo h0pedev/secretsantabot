fd2ca30edf39f7493189220b08a157084da890c5
hopelessDev <artur.valiev23@gmail.com>
2018-12-07 21:44:04 +0500

    test

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import telegram
-import urllib3
-from telegram.ext import Updater
+# import urllib3
+# from telegram.ext import Updater
 
 TOKEN = '609040137:AAHxqQK_4qIB4sLQgLJoGjT309R5Nc6Op4s'
-REQUEST_KWARGS = {
-    'proxy_url': 'http://62.80.178.24:34766'
-    # Optional, if you need authentication:
-    # 'urllib3_proxy_kwargs': {
-    #     'username': '3559738',
-    #     'password': '3559738',
-    # }
-}
-
-print(Updater(TOKEN, request_kwargs=REQUEST_KWARGS).bot.get_me())
+# REQUEST_KWARGS = {
+#     'proxy_url': 'http://62.80.178.24:34766'
+#     # Optional, if you need authentication:
+#     # 'urllib3_proxy_kwargs': {
+#     #     'username': '3559738',
+#     #     'password': '3559738',
+#     # }
+# }
 
 bot = telegram.Bot(TOKEN)
+
+print(bot.get_me())
